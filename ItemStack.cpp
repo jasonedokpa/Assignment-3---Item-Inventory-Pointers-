68db45361fffdfd5a754db133f7c949071825ee3
Jason Edokpa <jasonedokpa@gmail.com>
2022-03-14 16:02:51 -0500

    converted indentation to tabs

--- ItemStack.cpp
+++ ItemStack.cpp
@@ -8,101 +8,101 @@ const Item ItemStack::DEFAULT_ITEM(0, "Air");
 
 //------------------------------------------------------------------------------
 ItemStack::ItemStack()
-    :ItemStack(DEFAULT_ITEM, 0)
+	:ItemStack(DEFAULT_ITEM, 0)
 {
 }
 
 //------------------------------------------------------------------------------
 ItemStack::ItemStack(const Item& inputItem, int s)
-    :quantity(s)
+	:quantity(s)
 {
-    // Create a copy (clone) of inputItem and reference it with the this->item
-    // pointer
-    this->item = inputItem.clone();
+	// Create a copy (clone) of inputItem and reference it with the this->item
+	// pointer
+	this->item = inputItem.clone();
 }
 
 //------------------------------------------------------------------------------
 ItemStack::ItemStack(const ItemStack& src)
 {
-    // 1 . Create a copy (clone) of src.item and reference it with the
-    //     this->item pointer.
-        this->item = src.item->clone();
-    //
-    // 2. Do not forget to copy src.quantity.
-    //
-        this->quantity = src.quantity;
+	// 1 . Create a copy (clone) of src.item and reference it with the
+	//     this->item pointer.
+	//
+	this->item = src.item->clone();
 
+	// 2. Do not forget to copy src.quantity.
+	//
+	this->quantity = src.quantity;
 }
 
 //------------------------------------------------------------------------------
 ItemStack::~ItemStack()
 {
-    // Every pointer must be deleted to prevent memory leaks (item is a pointer).
-    delete this->item;
+	// Every pointer must be deleted to prevent memory leaks (item is a pointer).
+	delete this->item;
 }
 
 //------------------------------------------------------------------------------
 ItemStack& ItemStack::operator=(ItemStack rhs)
 {
-    swap(*this, rhs);
+	swap(*this, rhs);
 
-    return *this;
+	return *this;
 }
 
 //------------------------------------------------------------------------------
 Item ItemStack::getItem() const
 {
-    return *(this->item);
+	return *(this->item);
 }
 
 //------------------------------------------------------------------------------
 int ItemStack::size() const
 {
-    return this->quantity;
+	return this->quantity;
 }
 
 //------------------------------------------------------------------------------
 void ItemStack::addItems(int a)
 {
-    this->quantity += a;
+	this->quantity += a;
 }
 
 //------------------------------------------------------------------------------
 void ItemStack::addItemsFrom(const ItemStack& other)
 {
-    this->quantity += other.quantity;
+	this->quantity += other.quantity;
 }
 
 //------------------------------------------------------------------------------
 bool ItemStack::operator==(const ItemStack& rhs) const
 {
-    // Compare this and rhs for equivalence based on the ids of this->item and
-    // rhs.item.
-    //return false; // replace this line
-    return this->item->getID() == rhs.item->getID();
+	// Compare this and rhs for equivalence based on the ids of this->item and
+	// rhs.item.
+	//return false; // replace this line
+	return this->item->getID() == rhs.item->getID();
 }
 
 //------------------------------------------------------------------------------
 bool ItemStack::operator<(const ItemStack& rhs) const
 {
-    // Order (sort) this and rhs based on the ids of this->item and rhs.item.
-    //return false; // replace this line
-    return this->item->getID() < rhs.item->getID();
+	// Order (sort) this and rhs based on the ids of this->item and rhs.item.
+	//return false; // replace this line
+	return this->item->getID() < rhs.item->getID();
 }
 
 //------------------------------------------------------------------------------
 void ItemStack::display(std::ostream& outs) const
 {
-    outs << std::right << "(" << std::setw(2) << this->size() << ") "
-         << (this->getItem()).getName();
+	outs << std::right << "(" << std::setw(2) << this->size() << ") "
+		 << (this->getItem()).getName();
 }
 
 //------------------------------------------------------------------------------
 void swap(ItemStack& lhs, ItemStack& rhs)
 {
-    // Swap the item data members and quantity data members for lhs and rhs.
-    using std::swap;
+	// Swap the item data members and quantity data members for lhs and rhs.
+	using std::swap;
 
-    swap(lhs.item, rhs.item);
+	swap(lhs.item, rhs.item);
 	swap(lhs.quantity, rhs.quantity);
 }
