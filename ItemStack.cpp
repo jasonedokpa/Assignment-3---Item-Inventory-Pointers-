d683e7f6d1b9f022278b49bbddeb4d741c910ec3
Jason Edokpa <jasonedokpa@gmail.com>
2022-03-14 07:20:04 -0500

    initial

---
+++ ItemStack.cpp
@@ -0,0 +1,100 @@
+#include <iomanip>
+
+#include "ItemStack.h"
+
+
+const Item ItemStack::DEFAULT_ITEM(0, "Air");
+
+
+//------------------------------------------------------------------------------
+ItemStack::ItemStack()
+    :ItemStack(DEFAULT_ITEM, 0)
+{
+}
+
+//------------------------------------------------------------------------------
+ItemStack::ItemStack(const Item& inputItem, int s)
+    :quantity(s)
+{
+    // Create a copy (clone) of inputItem and reference it with the this->item
+    // pointer
+    this->item = inputItem.clone();
+}
+
+//------------------------------------------------------------------------------
+ItemStack::ItemStack(const ItemStack& src)
+{
+    // 1 . Create a copy (clone) of src.item and reference it with the
+    //     this->item pointer.
+    //
+    // 2. Do not forget to copy src.quantity.
+    //
+
+}
+
+//------------------------------------------------------------------------------
+ItemStack::~ItemStack()
+{
+    // Every pointer must be deleted to prevent memory leaks (item is a pointer).
+}
+
+//------------------------------------------------------------------------------
+ItemStack& ItemStack::operator=(ItemStack rhs)
+{
+    swap(*this, rhs);
+
+    return *this;
+}
+
+//------------------------------------------------------------------------------
+Item ItemStack::getItem() const
+{
+    return *(this->item);
+}
+
+//------------------------------------------------------------------------------
+int ItemStack::size() const
+{
+    return this->quantity;
+}
+
+//------------------------------------------------------------------------------
+void ItemStack::addItems(int a)
+{
+    this->quantity += a;
+}
+
+//------------------------------------------------------------------------------
+void ItemStack::addItemsFrom(const ItemStack& other)
+{
+    this->quantity += other.quantity;
+}
+
+//------------------------------------------------------------------------------
+bool ItemStack::operator==(const ItemStack& rhs) const
+{
+    // Compare this and rhs for equivalence based on the ids of this->item and
+    // rhs.item.
+    return false; // replace this line
+}
+
+//------------------------------------------------------------------------------
+bool ItemStack::operator<(const ItemStack& rhs) const
+{
+    // Order (sort) this and rhs based on the ids of this->item and rhs.item.
+    return false; // replace this line
+}
+
+//------------------------------------------------------------------------------
+void ItemStack::display(std::ostream& outs) const
+{
+    outs << std::right << "(" << std::setw(2) << this->size() << ") "
+         << (this->getItem()).getName();
+}
+
+//------------------------------------------------------------------------------
+void swap(ItemStack& lhs, ItemStack& rhs)
+{
+    // Swap the item data members and quantity data members for lhs and rhs.
+
+}
